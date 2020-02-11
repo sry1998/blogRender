287d084ac26d9db7cd9cde33fc95723368846a6f
shweta <sry9281@gmail.com>
2020-02-11 15:19:24 +0530

    changed value of req.user

--- middleware/token.js
+++ middleware/token.js
@@ -9,9 +9,11 @@ exports.verifyToken = function(req, res, next) {
   jwt.verify(token, config.secret, async function (err, decoded) {
     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     else {
-      req.id = decoded.id;
-      req.role = decoded.role;
-      req.name = decoded.name;
+      req.user = {
+        name: decoded.name,
+        id: decoded.id,
+        role: decoded.role
+      };
       next();
     }
   })
