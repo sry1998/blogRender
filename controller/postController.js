cb03c3b816652878b9eb93193c062aec95b7e3bb
shweta <sry9281@gmail.com>
2020-02-14 16:39:34 +0530

    Like feature added

--- controller/postController.js
+++ controller/postController.js
@@ -1,4 +1,5 @@
 const postModel = require('../model/postModel');
+const likeModel = require('../model/likeModel')
 
 exports.getDashboard = function (req, res) {
   req.flash('info', 'Welcome');
@@ -25,24 +26,45 @@ exports.getPost = async function (req, res) {
   try {
     if (req.role === "Admin") {
       const posts = await postModel.find();
-      if(posts) {
-        res.render('getpost',{post: posts});
+      if (posts) {
+        res.render('getpost', { post: posts, user:req.user.id });
       }
       else {
-        res.render('getpost',{error: "Nothing to show"});
+        res.render('getpost', { error: "Nothing to show" });
       }
     }
     else {
-      const posts = await postModel.find({ userid: req.user.id }, { _id: 0, userid: 0});
-      if(posts) {
-        res.render('getpost',{post: posts});
+      const posts = await postModel.find({ userid: req.user.id });
+      const likes = await likeModel.find();
+      if (posts) {
+        let btnval = req.flash('btnvalue') 
+        btnlike = 'Like';
+        res.render('getpost', { post: posts, like: likes, user:req.user});
       }
       else {
-        res.render('getpost',{error: "Nothing to show"});
+        res.render('getpost', { error: "Nothing to show" });
       }
-      
     }
   } catch (err) {
-    res.render('getpost',{error: "Something went wrong"});
+    res.render('getpost', { error: "Something went wrong" });
+  }
+}
+
+exports.getLikes = async function (req, res) {
+  try {
+    if (req.body.like == 'Like') {
+      await likeModel.updateOne({ postid: req.body.id, userid: req.user.id },
+        { $set: { like: true } },
+        { upsert: true });
+      res.redirect('/posts');
+    }
+    else {
+      await likeModel.updateOne({ postid: req.body.id, userid: req.user.id },
+        { $set: { like: false } });
+      res.redirect('/posts');
+    }
+  }
+  catch (err) {
+    res.render('getpost', { error: "Something went wrong" });
   }
 }
--- routes/routes.js
+++ routes/routes.js
@@ -20,6 +20,8 @@ router.post('/post',  middleware.verifyToken, postController.addPost);
 
 router.get('/posts', middleware.verifyToken, postController.getPost);
 
+router.post('/like', middleware.verifyToken, postController.getLikes);
+
 router.get('/logout', userController.logout);
 
 module.exports =  router;     
